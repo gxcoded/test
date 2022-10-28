2ac465a6986ec1b630af6b2abf77070fae986f97
gilgucela <82348697+gilgucela@users.noreply.github.com>
2022-10-29 00:12:20 +0800

    new commit

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server Running at Port " + PORT));
 
 app.get("/", (req, res) => {
-  res.status(200).send("THis is the Home Page for your API");
+  res.status(200).send("hello world");
 });
 console.log("hello world");
